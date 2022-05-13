4ee0e0cf93b6e0025edf1302ff9abdbbf37382f6
Martinchegem <martin.chege@student.moringaschool.com>
2022-05-13 23:20:22 +0300

    create a buttons: take the quiz, retake and next quiz

---
+++ index.js
@@ -0,0 +1,11 @@
+//variables names.. cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number
+//cannot contain a space or hyphen
+//case sensitive
+//
+let person = {
+    firstName: 'Martin',
+    lastName: 'Chege',
+    interestRate: 1
+};
